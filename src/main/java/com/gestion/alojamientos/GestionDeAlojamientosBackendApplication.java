e10069897a98eca7b00f6310a546d4eab531a99e
David Batero <davidbatero.r@gmail.com>
2025-08-21 13:32:59 -0500

    Creacion del proyecto

---
+++ src/main/java/com/gestion/alojamientos/GestionDeAlojamientosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.alojamientos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeAlojamientosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeAlojamientosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestion/alojamientos/GestionDeAlojamientosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion.alojamientos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeAlojamientosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
